d74813c874d44152316ddd25ce9c46b39faeca9b
Nate <nflynn4@wisc.edu>
2021-11-17 16:14:00 -0600

    Added second function OC.cpp

---
+++ cpp_code/OC.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+void OC(size_t nelx, size_t nely, double volfrac,double *x, double * dc) {
+//nelx is the number of elements in the x direction.
+//nely is the number of elements in the y direction. 
+//volfrac is the desired volume fraction.
+//x is an array of densities.
+//dc is ??
+ 
+int l1 = 0;
+int l2 = 100000;
+
+double optimal = 0.0001; 
+double move = 0.2; 
+
+while (l2-l1 > optimal) {
+	double lmid = 0.5 * (l2 + l1);
+	*xnew = max(0.0
+
+} 
+
+}
