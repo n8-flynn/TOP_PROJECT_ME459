c559934e0f246b0494422b1fce54a2595abdb881
Nate <nflynn4@wisc.edu>
2021-11-22 15:03:53 -0600

    Added CMakeLists.txt to be able to create a buildable for the file

--- cpp_code/OC.cpp
+++ cpp_code/OC.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <algorithm>
+
 double OC(size_t nelx, size_t nely, double volfrac,double *x, double * dc) {
-//nelx is the number of elements in the x direction.
-//nely is the number of elements in the y direction. 
-//volfrac is the desired volume fraction.
-//x is an array of densities.
-//dc is ??
+	//nelx is the number of elements in the x direction.
+	//nely is the number of elements in the y direction. 
+	//volfrac is the desired volume fraction.
+	//x is an array of densities.
+	//dc is ??
  
-int l1 = 0;
-int l2 = 100000;
+	int l1 = 0;
+	int l2 = 100000;
 
 double optimal = 0.0001; 
 double move = 0.2; 
