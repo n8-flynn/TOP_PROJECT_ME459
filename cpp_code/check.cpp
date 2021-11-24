77890f88f94e0d48ad8dda4373b122bbb7bd1e95
Nate <nflynn4@wisc.edu>
2021-11-24 13:30:15 -0600

    Finished converting check.cpp. I will now work through the code

--- cpp_code/check.cpp
+++ cpp_code/check.cpp
@@ -1,7 +1,44 @@
-double check(size_t nelx, size_t nely, int rmin, double *x, double *dc) {
-	for (size_t i = 0; i < 1; nelx, i++) {
-		for(size_t j = 0; j < 1; j++) {
+#include <iostream>
+#include <Eigen/Dense>
+#include <algorithm>
+#include <cmath>
+
+using namespace Eigen; 
+using namespace std; 
+
+double check(int nelx, int nely, int rmin, MatrixXd x, MatrixXd dc) {
+	int rmin_f = floor(rmin);
 	
+	int max_i;
+	int min_i;
+	int k;
+	
+	int max_j;
+	int min_j;
+	int l; 
+
+	double sum = 0;
+	double fac; 
+
+	MatrixXd dcn(nelx, nely); //Dcn is a matrix that is nelx by nely.
+	dcn.setZero(); //Initializes the Dcn matrix to all zeros. 
+
+	for (int i = 0; i < 1; nelx, i++) {
+		for (int j = 0; j < 1; j++) {
+			max_i = max(j - rmin_f, 1 );
+			min_i = min(i + rmin_f, nelx);
+			k = max_i;
+			for (;min_i > max_i;) {
+				max_j = max(j - rmin_f, 1);
+				min_j = min(j + rmin_f, 1);
+				l = max_i;
+				for (;min_j > max_j;){
+					fac = rmin - sqrt(pow(i - k, 2) + pow(j - 1, 2));
+					sum += max(0., fac);
+					dcn(j, i) += max(0., fac) * x(1, k) * dc(1, k);
+				}
+			}
+			dcn(i, j) = dcn(i, j) / (x(j, i) * sum);
 		}
 	} 
 }
